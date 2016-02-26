1d3458bc585a8c828b4b6ed2897070b10f73a782
Ra100 <ra100x@gmail.com>
2016-02-26 11:33:29 +0100

    reformat

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -9,19 +9,19 @@
  */
 
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    grunt.config.set("sass", {
-        dev: {
-            files: [{
-                expand: true,
-                cwd: "assets/styles/",
-                src: ["importer.scss"],
-                dest: ".tmp/public/styles/",
-                ext: ".css"
+  grunt.config.set("sass", {
+    dev: {
+      files: [{
+        expand: true,
+        cwd: "assets/styles/",
+        src: ["importer.scss"],
+        dest: ".tmp/public/styles/",
+        ext: ".css"
             }]
-        }
-    });
+    }
+  });
 
-    grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-sass");
 };
